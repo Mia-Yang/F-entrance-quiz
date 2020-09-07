e556e893a36dfd920fe32d68203647b363c12a9b
Yang Siyu <siyuyang1995@gmail.com>
2020-09-07 21:47:29 +0800

    [yang siyu]fix: modify the style of grouping block

--- src/App/components/Result/result.js
+++ src/App/components/Result/result.js
@@ -4,7 +4,7 @@ import './result.scss';
 class Result extends Component {
   render() {
     return (
-      <div>
+      <div className='group-block'>
         <h1>分组列表</h1>
         <button>分组学员</button>
       </div>
