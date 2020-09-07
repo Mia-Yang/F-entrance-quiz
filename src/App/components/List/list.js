d745665493138ed6f99b2d9bf59247bb236335f9
Yang Siyu <siyuyang1995@gmail.com>
2020-09-07 20:58:33 +0800

    [yang siyu]feat: get data from backend

--- src/App/components/List/list.js
+++ src/App/components/List/list.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import './list.scss';
 
 class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/list', { method: 'GET'})
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.setState({students: data})
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
   render() {
     return (
       <div>
         <h1>学员列表</h1>
-        <input type="text" placeholder={"+添加学员"}/>
+        <div className='list'>
+          {this.state.students.map((person) => 
+          <div className='personCard' key={person.id}>
+            {person.id}.{person.name}
+          </div>
+          )}
+        </div>
+        <input type="text" placeholder="+添加学员"/>
       </div>
     );
   }
